3248342e95beed239236b8aa39e6c2e9eea87f7f
Miles Mao <mrmao1@gmail.com>
2023-01-17 08:54:22 -0600

    variables exercise results

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is the temperature in Fahrenheit you want to convert?");
+            var fahTemp = Convert.ToDouble(Console.ReadLine());
+            var convertToCel = TempConverter.FahrenheitToCelsius(fahTemp);
+
+            Console.WriteLine($" That equals {convertToCel} degrees Celcius");
+            Console.WriteLine();
+
+            Console.WriteLine("What is the temperature in C you want to convert?");
+            var celTemp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"That equals {TempConverter.CelsiusToFahrenheit (celTemp)} degrees F");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+            var celTemp = (fTemp - 32) * (5.0 / 9.0); // to ensure it doesn't round the result to zero, use 5.0/9.0 instead of 5/9//
+            return celTemp;
+        }
+        
+        public static double CelsiusToFahrenheit(double cTemp)
+        {
+            var fahTemp = (cTemp * (9d / 5d)) + 32; // use the lowercase d notation like the m used in decimal to do the same as comment above//
+            return fahTemp;
+        }
+
+
+
+    }
+}
